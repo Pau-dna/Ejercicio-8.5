8454ca2d2f4ab5bc6940a74a6647759fcbd5ef7a
Paula Suarez <161689914+Pau-dna@users.noreply.github.com>
2024-08-18 14:39:02 -0500

    Add files via upload

---
+++ Personas/src/main/java/com/mycompany/personas/ListaPersonas.java
@@ -0,0 +1,37 @@
+package com.mycompany.personas;
+
+import java.util.*;
+
+public class ListaPersonas {
+    Vector listaPersonas; // Atributo que identifica un vector de personas
+    /**
+    * Constructor de la clase ListaPersonas
+    */
+    public ListaPersonas() {
+    listaPersonas = new Vector(); // Crea el vector de personas
+    }
+    /**
+    * Método que permite agregar una persona al vector de personas
+    * @param p Parámetro que define la persona a agregar al vector de
+    * personas
+    */
+    public void añadirPersona(Persona p) {
+listaPersonas.add(p);
+}
+/**
+* Método que permite eliminar una persona del vector de personas
+* @param i Parámetro que define la posición a eliminar en el vector
+* de personas
+*/
+public void eliminarPersona(int i) {
+listaPersonas.removeElementAt(i);
+}
+/**
+* Método que permite eliminar todos los elementos del vector de
+* personas
+*/
+public void borrarLista() {
+listaPersonas.removeAllElements();
+}
+    
+}
